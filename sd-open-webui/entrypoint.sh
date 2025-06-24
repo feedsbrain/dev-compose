8204f9419b39767ebc5b175f11e95ffca7528ade
Indra Gunawan <feedsbrain@gmail.com>
2025-06-24 21:27:08 +0800

    chore: cleanup script

--- sd-open-webui/entrypoint.sh
+++ sd-open-webui/entrypoint.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -x
 
-# reclone the repo if not exists
-if [ ! -d "/workdir/temp" ]; then
+# clone the repo if not exists
+if [ ! -f "/workdir/webui.sh" ]; then
   mkdir -p /workdir/temp
-  git clone --depth 1 --branch $1 https://github.com/AUTOMATIC1111/stable-diffusion-webui /workdir/temp/stable-diffusion-webui
+  git clone --depth 1 --branch $1 https://github.com/AUTOMATIC1111/stable-diffusion-webui /workdir/temp
+  mv /workdir/temp/.git /workdir
+  mv /workdir/temp/* /workdir
+  rm -rf /workdir/temp
 fi
-
-# copy update the original workdir
-cp -R -u -p /workdir/temp/stable-diffusion-webui/* /workdir
-
-# cleanup temp directory
-rm -rdf /workdir/temp
 
 # fix the permissions
 chown root:root -R /workdir
@@ -23,8 +20,7 @@ python3 -m venv venv
 # install extra dependencies and start webui
 . venv/bin/activate
 python3 -m pip config set global.cache-dir $PIP_CACHE_DIR
-python3 -m pip install --upgrade pip wheel
-python3 -m pip install basicsr
+python3 -m pip install --upgrade pip wheel basicsr
 
 # allow to run webui in root mode (for container)
 ./webui.sh -f
