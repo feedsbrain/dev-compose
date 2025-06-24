d18df70ff739295b27592912e90e99fd7e0b8ab0
Indra Gunawan <feedsbrain@gmail.com>
2025-06-24 19:15:10 +0800

    fix: clone script

--- sd-open-webui/entrypoint.sh
+++ sd-open-webui/entrypoint.sh
@@ -2,14 +2,16 @@
 set -x
 
 # reclone the repo if not exists
-if [ ! -d "/workdir/temp/stable-diffusion-webui" ]; then
+if [ ! -d "/workdir/temp" ]; then
   mkdir -p /workdir/temp
   git clone --depth 1 --branch $1 https://github.com/AUTOMATIC1111/stable-diffusion-webui /workdir/temp/stable-diffusion-webui
-  
-  # copy update the original workdir
-  cp -R -u -p /workdir/temp/stable-diffusion-webui/* /workdir
-  rm -rdf /workdir/temp
 fi
+
+# copy update the original workdir
+cp -R -u -p /workdir/temp/stable-diffusion-webui/* /workdir
+
+# cleanup temp directory
+rm -rdf /workdir/temp
 
 # fix the permissions
 chown root:root -R /workdir
